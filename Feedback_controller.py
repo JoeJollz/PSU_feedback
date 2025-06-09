24610553eda8556e00009dc5164e2495cd5c6818
JoeJollz <joseph.jolley@student.manchester.ac.uk>
2025-06-09 17:19:22 +0100

    Inital code fo Aim TTi controller

---
+++ Feedback_controller.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun  9 17:17:58 2025
+
+@author: jrjol
+"""
+
+import pyvisa
+import time
+
+# Open TCP/IP socket
+rm = pyvisa.ResourceManager()
+psu = rm.open_resource('TCPIP0::192.168.0.50::9221::SOCKET')  # Adjust IP and port
+
+# Query identification
+print(psu.query('*IDN?'))
+
+targ_power = 50.0 # Watts
+CHANNEL = 1 # CH1
+SLEEP_TIME = 0.5   # Time inbetween updates (seconds)
+
+psu.write(":VOLT 0.1") # set voltage to 0.1V
+psu.write(":CURR 10") # set current limit to 10A
+psu.write(":OUTP ON") #turn ON the output
+
+
+try:
+    while True:
+        voltage = float(psu.query(f'MEAS:VOLT? CH{CHANNEL}'))
+        current = float(psu.query(f'MEAS:CURR? CH{CHANNEL}'))
+        power = voltage * current
+        
+        
+        if voltage >0.5:
+            n_curr = targ_power/voltage
+            psu.write(f'CURR CH{CHANNEL},{n_curr:.3f}')
+        else:
+            n_curr = 0
+            psu.write(f'CURR CH{CHANNEL},0')
+        
+        
+        print(f"V{voltage:.2f}V, I={current:.2f}A, P={power:.2f}W -> curr_new={n_curr:.3f}A")
+        print('')
+        
+        
+        time.sleep(SLEEP_TIME)
+        
+except KeyboardInterrupt: # TO INTERUPT - PRESS Ctrl C
+    psu.write(f'OUTP CH{CHANNEL},OFF')
+    print("Stopped by user")
