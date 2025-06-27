f9824fb6b6405eb2562062f600ee00f771cdab26
JoeJollz <joseph.jolley@student.manchester.ac.uk>
2025-06-27 09:56:53 +0100

    Code ready for testing. Safety tests begin, and safety functions will be tested.

--- Wind_Power_Replication_FC.py
+++ Wind_Power_Replication_FC.py
@@ -21,7 +21,7 @@ def power_on(V, I):
     psu.write(f"VOLT {V}\n".encode())
     psu.write(f"CURR {I}\n".encode())
     psu.write(b"OUTP ON\n") #turn ON the output
-    time.sleep(0.2)
+    time.sleep(1)
     
 def power_update(P, volt_i, curr_i):
     curr_i_1 = targ_power/volt_i
@@ -111,7 +111,7 @@ try:
         
         
         v_i, c_i = measure()
-        if v_i > v_min: # projection from small voltages with high power - hence high current. Shut down.
+        if v_i > v_min: # voltage is sufficiently high - continue updating current.
             c_i_1 = power_update(targ_power, v_i, c_i)
         
         else: # projection from small voltages with high power - hence high current. Shut down.
@@ -146,74 +146,6 @@ except KeyboardInterrupt:
     plotting()
     
 
-targ_power = 9.0 # Watts
-SLEEP_TIME = 0.2   # Time inbetween updates (seconds)
-volt_mini = 0.5 # V
-MAX_CURRENT = 20# safety limit in A
-
-psu.write(b"VOLT 15\n") # set voltage to 0.1V
-psu.write(b"CURR 7\n") # set current limit to 10A
-psu.write(b"OUTP ON\n") #turn ON the output
-time.sleep(2) # 2seconds to allow for cuurent and voltage to be reached. 
-start_time = time.time()
-
-    
-try:
-    while True:
-        psu.write(b"MEAS:VOLT?\n")
-        volt_i = float(psu.readline().decode().strip())
-        
-        psu.write(b"MEAS:CURR?\n")
-        curr_i = float(psu.readline().decode().strip())
-        
-        elapsed_time = round(time.time() - start_time,2)
-        
-       
-        time_log.append(elapsed_time)
-        curr_log.append(curr_i)
-        volt_log.append(volt_i)
-        power_log.append(volt_i*curr_i)
-        
-        p = curr_i*volt_i 
-        data_log.append(f"{elapsed_time},{volt_i},{curr_i},{p}")
-        print(f"Time: {elapsed_time}s | Voltage: {volt_i}V | Current: {curr_i}A")
-        print(f"Power: {p}W")
-        print('----------------------------------------------------------------')
-        
-        
-        if volt_i >volt_mini: # Safety Barrier - avoids division by small voltages, hence leading to dangerously high currents.
-            curr_i_1 = targ_power/volt_i
-            command = f"CURR {curr_i_1}\n".encode()
-            psu.write(command)
-            print("Sent new current")
-        else: # if voltage is too low, current will be increased, hence becoming dangerous if increased too much. Current restricted to 0A.
-            psu.write(b"CURR 0\n")
-            psu.write(b"VOLT 0\n")
-            psu.write(b"OUTP OFF\n")
-            psu.close()
-            print(f'Automatic shut down. Voltage <{volt_mini}V, protected current becoming too large.')
-            plotting()
-        
-        if curr_i>MAX_CURRENT: # if current exceeds the maximum safety operating current - close off. 
-            psu.write(b"OUTP OFF\n")
-            print(f'Exceeded maximum current of {MAX_CURRENT}A')
-            plotting()
-        if elapsed_time>400:
-            psu.write(b"OUTP OFF\n")
-            psu.close()
-            plotting()
-            
-        time.sleep(SLEEP_TIME)
-        
-except KeyboardInterrupt: # TO INTERUPT - PRESS Ctrl C
-
-    psu.write(b"OUTP OFF\n")
-    print('Stopped logging data. 0A. 0V')
-    print("Stopped by user")
-    psu.close()
-    
-    plotting()
-    
 # file_path = r"C:\Users\jrjol\OneDrive - University of Cambridge\Documents\Cambridge PhD\Methanol Reforming Paper\GC data\NiALD_20Ce_C\data_log_14W.txt"
 
 # with open(file_path, 'w') as f:
